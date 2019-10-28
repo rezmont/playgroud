0cb345a2ee5563d8960e581d332fcb29da3218cf
rezmont <reza.motamedi@gmail.com>
2019-10-27 21:44:25 -0700

    iterations

---
+++ my-learn-with-test/integers/add.go
@@ -0,0 +1,5 @@
+package integers
+
+func add(a, b int) int {
+	return a + b
+}
--- my-learn-with-test/integers/add_test.go
+++ my-learn-with-test/integers/add_test.go
@@ -1,16 +1,35 @@
-import "testing"
+package integers
 
-func Test(t *testing.T) {
+import (
+	"fmt"
+	"testing"
+)
+
+func TestThis(t *testing.T) {
 	testCases := []struct {
-		desc string
+		a   int
+		b   int
+		sum int
 	}{
 		{
-			desc: "",
+			2, 2, 4,
+		},
+		{
+			2, -2, 0,
 		},
 	}
 	for _, tC := range testCases {
-		t.Run(tC.desc, func(t *testing.T) {
-
+		t.Run("", func(t *testing.T) {
+			s := add(tC.a, tC.b)
+			if tC.sum != s {
+				t.Errorf("wanted %v, got %v", tC.sum, s)
+			}
 		})
 	}
+}
+
+func ExampleAdd() {
+	sum := add(1, 109)
+	fmt.Println(sum)
+	// outpu 110
 }
